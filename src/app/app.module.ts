81b1183f6dba7cc3df8cd9394ab4a0ae914710c4
ByMartrixx <bymartrixx@gmail.com>
2020-12-13 17:43:35 -0300

    Add Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -23,7 +24,8 @@ import { ProjectComponent } from './projects/project/project.component';
     HttpClientModule,
     HttpClientJsonpModule,
     NgbModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
